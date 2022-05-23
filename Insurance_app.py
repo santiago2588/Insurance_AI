79a0ac49c00fd516064875d0683b6b4b633c5039
Santiago Zapata <72705458+santiago2588@users.noreply.github.com>
2022-05-23 22:48:31 +0100

    Update Insurance_app.py

--- Insurance_app.py
+++ Insurance_app.py
@@ -37,8 +37,6 @@ def run():
     st.sidebar.info('This app is created to predict patient hospital charges')
     st.sidebar.success('https://www.pycaret.org')
     
-    st.sidebar.image(image_hospital)
-
     st.title("Insurance Charges Prediction App")
 
     if add_selectbox == 'Online':
